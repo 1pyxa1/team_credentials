8d2706d8bd0665cde176d8a5776b0ea8e4fea2c9
1pyxa1 <1pyxa1@gmail.com>
2021-05-27 15:12:55 +0300

    refactoring

--- team_credentials/team_credentials.py
+++ team_credentials/team_credentials.py
@@ -1,12 +1,13 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from base64 import b64encode, b64decode
 import json
-from Crypto.Protocol.KDF import scrypt
-from pathlib import Path
 import re
+from base64 import b64decode, b64encode
+from pathlib import Path
+from typing import Dict, Union
+
 import requests
-from typing import Dict
+from Crypto.Cipher import AES, _mode_gcm
+from Crypto.Protocol.KDF import scrypt
+from Crypto.Random import get_random_bytes
 
 
 class AesGcm:
@@ -14,29 +15,42 @@ class AesGcm:
         self.json_k = ['nonce', 'ciphertext', 'tag', 'salt']
 
     def encryption(self, password: str, msg: str) -> str:
-        salt = get_random_bytes(32).decode('utf8')
-        keys = scrypt(password, salt, key_len=32, N=2**17, r=8, p=1, num_keys=1)
-
-        if not isinstance(keys, bytes):
-            key = keys[0]
-        else:
-            key = keys
+        salt = get_random_bytes(32)
+        key = scrypt(
+            password, b64encode(salt).decode('utf8'), key_len=32, N=2**17, r=8, p=1, num_keys=1)
+        if not isinstance(key, bytes):
+            raise TypeError
 
         cipher = AES.new(key, AES.MODE_GCM)
+        if not isinstance(cipher, _mode_gcm.GcmMode):
+            raise TypeError
+
         ciphertext, tag = cipher.encrypt_and_digest(msg.encode('utf8'))
+
         json_v = [b64encode(x).decode('utf-8') for x in (cipher.nonce, ciphertext, tag, salt)]
+
         encrypted_msg = json.dumps(dict(zip(self.json_k, json_v)))
         return encrypted_msg
 
-    def decryption(self, encrypted_msg: str, password: str) -> str:
+    def decryption(self, password: str, encrypted_msg: str) -> str:
 
         b64 = json.loads(encrypted_msg)
-        jv = {k: b64decode(b64[k]) for k in self.json_k}
+        json_v = {}
+        for key in self.json_k:
+            if key == 'salt':
+                json_v[key] = b64[key]
+            else:
+                json_v[key] = b64decode(b64[key])
 
-        key = scrypt(password, jv['salt'].decode('utf8'), key_len=32, N=2**17, r=8, p=1)
+        key = scrypt(password, json_v['salt'], key_len=32, N=2**17, r=8, p=1, num_keys=1)
+        if not isinstance(key, bytes):
+            raise TypeError
 
-        cipher = AES.new(key, AES.MODE_GCM, nonce=jv['nonce'])
-        plaintext = cipher.decrypt_and_verify(jv['ciphertext'], jv['tag'])
+        cipher = AES.new(key, AES.MODE_GCM, nonce=json_v['nonce'])
+        if not isinstance(cipher, _mode_gcm.GcmMode):
+            raise TypeError
+
+        plaintext = cipher.decrypt_and_verify(json_v['ciphertext'], json_v['tag'])
         return plaintext.decode('utf8')
 
 
@@ -48,38 +62,19 @@ class TeamCredentials:
 
         self.aes = AesGcm()
 
-        self.url = self.conf['url']
-        self.team_master_password = self.conf['team_master_password']
-        self.team_master_salt = self.conf['team_master_salt']
-        self.credential = self.conf['credential']
-        self.master_password = self.conf['master_password']
-        self.master_salt = self.conf['master_salt']
-
         self.team_json_fn = 'team_credential.json'
-        self.url_team = self.url + f'{self.team_json_fn}/raw?ref=master'
+        self.url_team = self.conf['url'] + f'{self.team_json_fn}/raw?ref=master'
 
-        self.cred_json_fn = f'{self.credential}_credential.json'
-        self.url_cred = self.url + f'{self.cred_json_fn}/raw?ref=master'
+        self.cred_json_fn = f'{self.conf["credential"]}_credential.json'
+        self.url_cred = self.conf['url'] + f'{self.cred_json_fn}/raw?ref=master'
 
         self.gitlab_personal_token = self.conf['gitlab_personal_token']
 
     def _read_conf(self, conf: Path):
-        with open(conf) as jf:
-            self.conf = json.load(jf)
-
-    def create(self):
-        pass
+        with open(conf) as json_f:
+            self.conf = json.load(json_f)
 
     def check(self):
-        pass
-
-    def update(self):
-        pass
-
-    def bulk_update(self):
-        pass
-
-    def delete(self):
         pass
 
     def _get_credentials(self):
@@ -94,11 +89,11 @@ class TeamCredentials:
         return team_resp.json(), cred_resp.json()
 
     def _get_local_credentials(self):
-        with open(self.team_json_fn) as fn:
-            team_resp = json.load(fn)
+        with open(self.team_json_fn) as file_n:
+            team_resp = json.load(file_n)
 
-        with open(self.cred_json_fn) as fn:
-            cred_resp = json.load(fn)
+        with open(self.cred_json_fn) as file_n:
+            cred_resp = json.load(file_n)
 
         return team_resp, cred_resp
 
@@ -118,16 +113,38 @@ class TeamCredentials:
 
     def encrypt(self, msg: str, public: bool = False):
         if public:
-            return self.aes.encryption(self.team_master_password, msg + self.team_master_salt)
-        return self.aes.encryption(self.master_password, msg + self.master_salt)
+            return self.aes.encryption(
+                self.conf['team_master_password'], msg + self.conf['team_master_salt'])
+        return self.aes.encryption(self.conf['master_password'], msg + self.conf['master_salt'])
+
+    def encrypt_cred(
+            self,
+            cred: str,
+            dict_msg: Dict[str, Dict[str, Union[str, int]]],
+            only_cred: bool = False
+            ) -> Union[tuple, str]:
+        team_dic: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = {cred: {}}
+        cred_dic: Dict[str, Dict[str, Dict[str, Union[str, int]]]] = {cred: {}}
+
+        for key, val in dict_msg.items():
+            if not isinstance(val['value'], str):
+                raise TypeError
+
+            if val['public'] == 1 and only_cred is False:
+                team_dic[cred][key] = {'public': 1}
+                team_dic[cred][key]['value'] = json.loads(self.encrypt(val['value'], public=True))
+            elif val['public'] == 0:
+                team_dic[cred][key] = {'public': 0}
+                cred_dic[cred][key] = json.loads(self.encrypt(val['value'], public=False))
+
+        if only_cred:
+            return json.dumps(cred_dic, indent=4)
+        return json.dumps(team_dic, indent=4), json.dumps(cred_dic, indent=4)
 
     def decrypt(self, encrypted_msg: str, public: bool = False):
         if public:
-            msg = self.aes.decryption(encrypted_msg, self.team_master_password)
-            return re.sub(re.escape(self.team_master_salt) + '$', '', msg)
+            msg = self.aes.decryption(self.conf['team_master_password'], encrypted_msg)
+            return re.sub(re.escape(self.conf['team_master_salt']) + '$', '', msg)
 
-        msg = self.aes.decryption(encrypted_msg, self.master_password)
-        return re.sub(re.escape(self.master_salt) + '$', '', msg)
-
-    def get_credentials_backup(self):
-        pass
+        msg = self.aes.decryption(self.conf['master_password'], encrypted_msg)
+        return re.sub(re.escape(self.conf['master_salt']) + '$', '', msg)
